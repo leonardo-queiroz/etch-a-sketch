5affb40af3e7bb4e20d70c01e10ec624b2fde686
Leonardo Queiroz <leonardobcq@outlook.com>
2023-02-16 15:39:14 -0300

    Create sliders to adjust brush and canvas' size

--- script.js
+++ script.js
@@ -1,42 +1,12 @@
 const body = document.querySelector("body");
+const r = document.querySelector(":root");
 const canvas = document.querySelector("#canvas");
-let canvasWidth = 500;
-let canvasHeight = 500;
 
-const activeColor = document.querySelector("#active-color");
-const canvasColor = document.querySelector("#canvas-color");
-canvasColor.addEventListener("input", function() {
-    canvas.style.backgroundColor = canvasColor.value;
-})
-
-const smallButton = document.querySelector("#small-button");
-smallButton.addEventListener("click", function() {
-    canvasWidth = 400;
-    canvasHeight = 400;
-    canvas.removeAttribute("class");
-    canvas.classList.add("s-canvas");
-    renderCanvas();
-});
-const mediumButton = document.querySelector("#medium-button");
-mediumButton.addEventListener("click", function() {
-    canvasWidth = 500;
-    canvasHeight = 500;
-    canvas.removeAttribute("class");
-    canvas.classList.add("m-canvas");
-    renderCanvas();
-});
-const largeButton = document.querySelector("#large-button");
-largeButton.addEventListener("click", function() {
-    canvasWidth = 600;
-    canvasHeight = 600;
-    canvas.removeAttribute("class");
-    canvas.classList.add("l-canvas");
-    renderCanvas();
-});
-
-let squaresPerSide = 50;
+let canvasWidth = 450;
+let canvasHeight = 450;
 let squaresWidth;
 let squaresHeight;
+let squaresPerSide = 50;
 
 let holdClick = false;
 body.addEventListener("mousedown", function() {
@@ -46,17 +16,85 @@ body.addEventListener("mouseup", function() {
     holdClick = false;
 })
 
+const activeColor = document.querySelector("#active-color");
+activeColor.addEventListener("change", function() {
+    r.style.setProperty('--brushColor', activeColor.value);
+});
+
+const canvasColor = document.querySelector("#canvas-color");
+canvasColor.addEventListener("input", function() {
+    r.style.setProperty('--canvasColor', canvasColor.value);
+});
+
+let randomOn = false;
+const randomButton = document.querySelector("#random-button");
+randomButton.addEventListener("click", function() {
+    if (!randomOn) {
+        randomOn = true;
+    } else if (randomOn) {
+        randomOn = false;
+    }
+});
+
+let gridOn = false;
+const gridButton = document.querySelector("#grid-button");
+gridButton.addEventListener("click", function() {
+    const pixels = document.querySelectorAll(".pixels");
+    
+    if (!gridOn) {
+        gridOn = true;
+        pixels.forEach(pixel => pixel.classList.add("pixel-grid"));
+    } else if (gridOn) {
+        gridOn = false;
+        pixels.forEach(pixel => pixel.classList.remove("pixel-grid"));
+    }
+});
+
+let eraserOn = false;
+const eraserButton = document.querySelector("#eraser-button");
+eraserButton.addEventListener("click", function() {
+    if (!eraserOn) {
+        eraserOn = true;
+    } else if (eraserOn) {
+        eraserOn = false;
+    }
+});
+
+const brushSlider = document.querySelector("#brush-slider");
+brushSlider.addEventListener("change", function() {
+    squaresPerSide = brushSlider.value;
+
+    renderCanvas();
+})
+
+const canvasSlider = document.querySelector("#canvas-slider");
+canvasSlider.addEventListener("change", function() {
+    canvasWidth = canvasSlider.value;
+    canvasHeight = canvasSlider.value;
+
+    renderCanvas();
+})
+
+
 renderCanvas();
 
+
 function renderCanvas() {
-    canvas.textContent = "";  
+    canvas.textContent = "";
+    r.style.setProperty('--canvasWidth', `${canvasWidth}px`);
+    r.style.setProperty('--canvasHeight', `${canvasHeight}px`);
 
     calculateSquare();
 
     for (let i = 1; i <= squaresPerSide ** 2; i++) {
         const pixel = document.createElement("div");
         pixel.removeAttribute("class");
-        pixel.classList.add("pixels");     
+        pixel.classList.add("pixels");
+        
+        if (gridOn) {
+            pixel.classList.add("pixel-grid");
+        }
+
         pixel.style.width = `${squaresWidth}px`;
         pixel.style.height = `${squaresHeight}px`;
         pixel.addEventListener("mousedown", paint);
@@ -74,5 +112,18 @@ function paint(e) {
     if (e.type === "mouseover" && !holdClick) {
         return;
     }
-    e.target.style.backgroundColor = activeColor.value;
+
+    if (eraserOn) {
+        e.target.style.backgroundColor = "";
+    } 
+    else if (randomOn) {
+        let R = Math.floor(Math.random() * 256);
+        let G = Math.floor(Math.random() * 256);
+        let B = Math.floor(Math.random() * 256);
+        
+        e.target.style.backgroundColor = `rgb(${R}, ${G}, ${B})`;
+    } 
+    else {
+        e.target.style.backgroundColor = activeColor.value;
+    }
 }
